960aa41fbe5b78794d9d14473cbd2d9235b874bd
aorlanova <nina.orlanova88@gmail.com>
2022-08-15 16:03:54 -0400

    aug 15 update, file explorer

--- src/testingCenter/ExplorerFile.java
+++ src/testingCenter/ExplorerFile.java
@@ -1,6 +1,9 @@
 package testingCenter;
 import java.util.*;
+import java.nio.*;
 import java.io.*;
+import java.util.stream.*;
+import java.nio.file.*;
 
 public class ExplorerFile {
 
@@ -9,18 +12,39 @@ public class ExplorerFile {
 		File testFile = new File("dooboo.txt");
 		String absPath = testFile.getAbsolutePath();
 		System.out.println(absPath);
+		List<String> wordFileList = new ArrayList<>();
+		Map <Integer, Long> result = new TreeMap<>();
+		
+		
 		try  {
 			
 			System.out.println("file found");
 			FileInputStream fin  = new FileInputStream(testFile);
 			byte byteData;
 			int i = 0;
+			Scanner in = new Scanner(testFile);
+			
 			while (((byteData = (byte) fin.read()) != -1) || (i==10)) {
 				
 				System.out.print((char) byteData);
 				i++;
+				
 			}
 			
+			while (in.hasNext()) {
+				
+				wordFileList.add(in.next());
+				
+			}
+			
+			Stream<String> words = wordFileList.stream();
+			result = words.collect(Collectors.groupingBy(w -> w.length(), 
+					Collectors.counting()));
+					
+			System.out.println("The amount of words and their lengths in dooboo.txt:");
+			System.out.println(result);
+			
+			in.close();
 			fin.close();
 			
 			

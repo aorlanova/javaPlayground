c705879ede93eb48c2e3347ede5383a981283f97
aorlanova <nina.orlanova88@gmail.com>
2022-08-15 16:10:11 -0400

    Aug 15 fileexplorer

--- src/testingCenter/ExplorerFile.java
+++ src/testingCenter/ExplorerFile.java
@@ -9,6 +9,7 @@ public class ExplorerFile {
 
 	public static void main(String[] args) throws IOException {
 		
+		// testFile is created, dooboo.txt
 		File testFile = new File("dooboo.txt");
 		String absPath = testFile.getAbsolutePath();
 		System.out.println(absPath);
@@ -16,14 +17,17 @@ public class ExplorerFile {
 		Map <Integer, Long> result = new TreeMap<>();
 		
 		
+	
 		try  {
-			
+			// file exists
 			System.out.println("file found");
+			// file input stream, fin made to print every word in file, dooboo.txt
 			FileInputStream fin  = new FileInputStream(testFile);
 			byte byteData;
 			int i = 0;
 			Scanner in = new Scanner(testFile);
 			
+			// while fin.read() does not return -1, every byte, type-casted to chat is printed.
 			while (((byteData = (byte) fin.read()) != -1) || (i==10)) {
 				
 				System.out.print((char) byteData);
@@ -31,12 +35,14 @@ public class ExplorerFile {
 				
 			}
 			
+			// all words are added to the List of words in dooboo.txt
 			while (in.hasNext()) {
 				
 				wordFileList.add(in.next());
 				
 			}
 			
+			// Stream to sort words by length.
 			Stream<String> words = wordFileList.stream();
 			result = words.collect(Collectors.groupingBy(w -> w.length(), 
 					Collectors.counting()));
@@ -50,6 +56,7 @@ public class ExplorerFile {
 			
 		} catch (FileNotFoundException e){
 			
+			// file created if dooboo.txt not found.
 			System.out.println("file not found");
 			testFile.createNewFile();
 			System.out.println("file created " + testFile.getAbsolutePath());
